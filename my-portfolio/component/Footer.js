d7139356288bed6a6fb2b50edb3ba4947737f166
Ignacio <ignmonzon@gmail.co>
2021-06-05 20:55:41 -0300

    footer2

--- my-portfolio/component/Footer.js
+++ my-portfolio/component/Footer.js
@@ -43,23 +43,47 @@ const Footer = () => {
               <h2 >Mis redes</h2>
               <p>orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
             </div>
+            <div className="container">
             <form className="formulario">
+                <div className="contactos">
+                <div className="boton-areaf">
+                      <a className="boton2">Facebook</a>
+                    </div>
+                    <div className="boton-areaf">
+                      <a className="boton2">Github</a>
+                    </div>
+                    <div className="boton-areaf">
+                      <a className="boton2">Linkedin</a>
+                    </div>
+                    <div className="boton-areaf">
+                      <a className="boton2">Gmail</a>
+                    </div>
+                    <div className="boton-areaf">
+                      <a className="boton2">Telefono</a>
+                    </div>
+
+                </div>
+                <div className="formularioInt2">
               <div className="formularioInt">
-                <label htmlFor="name">nombre</label>
-                <input type="text" name="name" className="inputField" onChange={(e) => { setName(e.target.value) }} ></input>
+                <label htmlFor="name">Nombre</label>
+                <input type="text" name="name" className="inputField" placeholder="Ingresa tu Nombre" onChange={(e) => { setName(e.target.value) }} ></input>
               </div>
               <div className="formularioInt">
                 <label htmlFor="email">Email</label>
-                <input type="email" name="email" className="inputField" onChange={(e) => {setEmail(e.target.value) }} ></input>
+                <input type="email" name="email" className="inputField" placeholder="Ingresa tu Email" onChange={(e) => {setEmail(e.target.value) }} ></input>
               </div>
               <div className="formularioInt">
                 <label htmlFor="texto" >Menasaje</label>
-                <textarea type="text" name="texto" className="inputField" onChange={(e) => { setTexto(e.target.value) }} ></textarea>
+                <textarea type="text" name="texto" className="inputField2"  placeholder="Envia tu mensaje" onChange={(e) => { setTexto(e.target.value) }} ></textarea>
               </div>
-              <div >
-              < input type='submit' onClick={(e)=>{handleSubmit(e)}}/>
+              <div className="areas">
+              <div className="boton-areaf">
+                      <a className="boton" type="submit"  onClick={(e)=>{handleSubmit(e)}} >contactame</a>
+                    </div>
+                    </div>
               </div>
             </form>
+            </div>
           </div>
         </div>
 
